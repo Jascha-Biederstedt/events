2047074da6cb451f717a49d44ebb3fb49da36f07
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-05-23 11:27:28 +0200

    Add event items to home page

---
+++ components/events/EventList.js
@@ -0,0 +1,20 @@
+import EventItem from './EventItem';
+
+export default function EventList(props) {
+  const { items } = props;
+
+  return (
+    <ul>
+      {items.map(event => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          date={event.date}
+          location={event.location}
+          image={event.image}
+        />
+      ))}
+    </ul>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { getFeaturedEvents } from '../dummyData';
+import EventList from '../components/events/EventList';
+
 export default function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>The Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
