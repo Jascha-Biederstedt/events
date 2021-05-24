c1d99d6a792997c96232da5901f0f883a74ff296
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-05-24 09:34:26 +0200

    Add the event detail page

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,6 +1,9 @@
-import Link from 'next/link';
-
 import classes from './EventItem.module.css';
+
+import Button from '../ui/Button';
+import DateIcon from '../icons/DateIcon';
+import AddressIcon from '../icons/AddressIcon';
+import ArrowRightIcon from '../icons/ArrowRightIcon';
 
 export default function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -20,14 +23,21 @@ export default function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,7 +1,33 @@
+import { Fragment } from 'react';
+
+import { useRouter } from 'next/router';
+
+import { getEventById } from '../../dummyData';
+import EventContent from '../../components/eventDetail/EventContent';
+import EventSummary from '../../components/eventDetail/EventSummary';
+import EventLogistics from '../../components/eventDetail/EventLogistics';
+
 export default function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>The Event Detail Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
