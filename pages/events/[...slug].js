a0ea48f179dd911f3ddeb3f0b359b020362b5ea6
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-05-24 11:23:58 +0200

    Render filtered events

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,55 @@
+import { useRouter } from 'next/router';
+
+import { getFilteredEvents } from '../../dummyData';
+import EventList from '../../components/events/EventList';
+
 export default function FilteredEventsPage() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return (
+      <p className="center" style={{ marginTop: '2rem' }}>
+        Loading...
+      </p>
+    );
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    <p style={{ marginTop: '2rem' }}>
+      Invalid filter. Please adjust your values.
+    </p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <p className="center" style={{ marginTop: '2rem' }}>
+        No events found.
+      </p>
+    );
+  }
+
   return (
     <div>
-      <h1>The Filtered Events Page</h1>
+      <EventList items={filteredEvents} />
     </div>
   );
 }
