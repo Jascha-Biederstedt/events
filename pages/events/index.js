84218d1e3e7770c26e452cbc0f9c80c3020aeba4
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-05-24 10:45:28 +0200

    Implement programmatic navigation for filtered events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,23 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
 import { getAllEvents } from '../../dummyData';
 import EventList from '../../components/events/EventList';
+import EventsSearch from '../../components/events/EventsSearch';
 
 export default function EventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
 
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
+
   return (
-    <div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 }
